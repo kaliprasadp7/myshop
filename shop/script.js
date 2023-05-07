b33f7ffc7f1af6cde1b7ec97ed94badc94def59c
Kali Prasad Panda <kalip.panda@nettantra.net>
2023-05-08 00:24:53 +0530

    search bar and filters functionality added

--- shop/script.js
+++ shop/script.js
@@ -1,7 +1,15 @@
+const filters = document.querySelectorAll(".filter");
 const mens_clothing_div = document.getElementById("mens-clothing");
 const womens_clothing_div = document.getElementById("Womens-clothing");
 const jewellery_div = document.getElementById("jewellery");
 const electronics_div = document.getElementById("electronics");
+
+const mens_section = document.getElementById("mens-section");
+const womens_section = document.getElementById("womens-section");
+const jewellery_section = document.getElementById("jewellery-section");
+const electronics_section = document.getElementById("electronics-section");
+
+const search_bar = document.getElementById("search-bar");
 
 if (!localStorage.getItem('currentUser')) {
   // Redirect user to shop page if accesstoken exists and user is trying to access signup page
@@ -14,6 +22,7 @@ fetch('https://fakestoreapi.com/products').then(response => response.json()).the
       mens_clothing_div.innerHTML += `<div class="item">
       <img src="${item.image}" alt="Item" />
       <div class="info">
+      <div class="product-name">${item.title}</div>
         <div class="row">
           <div class="price">$${item.price}</div>
           <div class="sized">S,M,L</div>
@@ -36,6 +45,7 @@ fetch('https://fakestoreapi.com/products').then(response => response.json()).the
       womens_clothing_div.innerHTML += `<div class="item">
       <img src="${item.image}" alt="Item" />
       <div class="info">
+      <div class="product-name">${item.title}</div>
         <div class="row">
           <div class="price">$${item.price}</div>
           <div class="sized">S,M,L</div>
@@ -58,6 +68,7 @@ fetch('https://fakestoreapi.com/products').then(response => response.json()).the
       jewellery_div.innerHTML += `<div class="item">
       <img src="${item.image}" alt="Item" />
       <div class="info">
+      <div class="product-name">${item.title}</div>
         <div class="row">
           <div class="price">$${item.price}</div>
           <div class="sized">S,M,L</div>
@@ -80,6 +91,7 @@ fetch('https://fakestoreapi.com/products').then(response => response.json()).the
       electronics_div.innerHTML += `<div class="item">
       <img src="${item.image}" alt="Item" />
       <div class="info">
+      <div class="product-name">${item.title}</div>
         <div class="row">
           <div class="price">$${item.price}</div>
           <div class="sized">S,M,L</div>
@@ -98,5 +110,59 @@ fetch('https://fakestoreapi.com/products').then(response => response.json()).the
     </div>`;
     }
 
+    //filter items by name using search bar
+    search_bar.addEventListener("keyup", function() {
+      const search_value = this.value.toLowerCase();
+    
+      // Loop through all items and hide/show based on search value
+      const items = document.querySelectorAll(".item");
+      items.forEach(item => {
+        const title = item.querySelector(".product-name").innerText.toLowerCase();
+        if (title.includes(search_value)) {
+          item.style.display = "block";
+        } else {
+          item.style.display = "none";
+        }
+      });
+    });
+
   })
 })
+
+//filtering
+
+filters.forEach(filter => {
+  filter.addEventListener("click", () => {
+    filters.forEach(f => f.classList.remove("active"));
+    filter.classList.add("active");
+
+    // Show or hide the corresponding sections based on the selected filter
+    if (filter.innerText === "All") {
+      mens_section.style.display = "block";
+      womens_section.style.display = "block";
+      jewellery_section.style.display = "block";
+      electronics_section.style.display = "block";
+    } else if (filter.innerText === "Mens") {
+      mens_section.style.display = "block";
+      womens_section.style.display = "none";
+      jewellery_section.style.display = "none";
+      electronics_section.style.display = "none";
+    } else if (filter.innerText === "Womens") {
+      mens_section.style.display = "none";
+      womens_section.style.display = "block";
+      jewellery_section.style.display = "none";
+      electronics_section.style.display = "none";
+    } else if (filter.innerText === "Jewellery") {
+      mens_section.style.display = "none";
+      womens_section.style.display = "none";
+      jewellery_section.style.display = "block";
+      electronics_section.style.display = "none";
+    } else if (filter.innerText === "Electronics") {
+      mens_section.style.display = "none";
+      womens_section.style.display = "none";
+      jewellery_section.style.display = "none";
+      electronics_section.style.display = "block";
+    }
+  });
+});
+
