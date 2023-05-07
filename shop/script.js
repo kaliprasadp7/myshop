7247e39209533107638a503501142d687bcac74e
Kali Prasad Panda <kalip.panda@nettantra.net>
2023-05-07 21:44:05 +0530

    fetched products of shop page using api

--- shop/script.js
+++ shop/script.js
@@ -1,10 +1,102 @@
-const produtc = {
-  id: 1,
-  title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-  price: 109.95,
-  description:
-    "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-  category: "men's clothing",
-  image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-  rating: { rate: 3.9, count: 120 },
-};
+const mens_clothing_div = document.getElementById("mens-clothing");
+const womens_clothing_div = document.getElementById("Womens-clothing");
+const jewellery_div = document.getElementById("jewellery");
+const electronics_div = document.getElementById("electronics");
+
+if (!localStorage.getItem('currentUser')) {
+  // Redirect user to shop page if accesstoken exists and user is trying to access signup page
+  window.location.href = '/';
+}
+
+fetch('https://fakestoreapi.com/products').then(response => response.json()).then(data => {
+  data.forEach((item)=>{
+    if (item.category === "men's clothing") {
+      mens_clothing_div.innerHTML += `<div class="item">
+      <img src="${item.image}" alt="Item" />
+      <div class="info">
+        <div class="row">
+          <div class="price">$${item.price}</div>
+          <div class="sized">S,M,L</div>
+        </div>
+        <div class="colors">
+          Colors:
+          <div class="row">
+            <div class="circle" style="background-color: #000"></div>
+            <div class="circle" style="background-color: #4938af"></div>
+            <div class="circle" style="background-color: #203d3e"></div>
+          </div>
+        </div>
+        <div class="row">Rating:${item.rating.rate}</div>
+      </div>
+      <button id="addBtn">Add to Cart</button>
+    </div>`;
+    }
+
+    if (item.category === "women's clothing") {
+      womens_clothing_div.innerHTML += `<div class="item">
+      <img src="${item.image}" alt="Item" />
+      <div class="info">
+        <div class="row">
+          <div class="price">$${item.price}</div>
+          <div class="sized">S,M,L</div>
+        </div>
+        <div class="colors">
+          Colors:
+          <div class="row">
+            <div class="circle" style="background-color: #000"></div>
+            <div class="circle" style="background-color: #4938af"></div>
+            <div class="circle" style="background-color: #203d3e"></div>
+          </div>
+        </div>
+        <div class="row">Rating:${item.rating.rate}</div>
+      </div>
+      <button id="addBtn">Add to Cart</button>
+    </div>`;
+    }
+
+    if (item.category === "jewelery") {
+      jewellery_div.innerHTML += `<div class="item">
+      <img src="${item.image}" alt="Item" />
+      <div class="info">
+        <div class="row">
+          <div class="price">$${item.price}</div>
+          <div class="sized">S,M,L</div>
+        </div>
+        <div class="colors">
+          Colors:
+          <div class="row">
+            <div class="circle" style="background-color: #000"></div>
+            <div class="circle" style="background-color: #4938af"></div>
+            <div class="circle" style="background-color: #203d3e"></div>
+          </div>
+        </div>
+        <div class="row">Rating:${item.rating.rate}</div>
+      </div>
+      <button id="addBtn">Add to Cart</button>
+    </div>`;
+    }
+
+    if (item.category === "electronics") {
+      electronics_div.innerHTML += `<div class="item">
+      <img src="${item.image}" alt="Item" />
+      <div class="info">
+        <div class="row">
+          <div class="price">$${item.price}</div>
+          <div class="sized">S,M,L</div>
+        </div>
+        <div class="colors">
+          Colors:
+          <div class="row">
+            <div class="circle" style="background-color: #000"></div>
+            <div class="circle" style="background-color: #4938af"></div>
+            <div class="circle" style="background-color: #203d3e"></div>
+          </div>
+        </div>
+        <div class="row">Rating:${item.rating.rate}</div>
+      </div>
+      <button id="addBtn">Add to Cart</button>
+    </div>`;
+    }
+
+  })
+})
